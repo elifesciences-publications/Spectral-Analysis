3aa3399ed7201ee2f6f0a1f7c723142f38d378f8
Avinash Pujala <avinashpujala@gmail.com>
2015-01-31 18:21:02 -0500

    ComputeMultibandXWT: In addition to computing the XWT of a signal, also computes a version of the XWT where for each time point only the frequency peaks are shown

--- Cross wavelet/ComputeMultibandXWT.m
+++ Cross wavelet/ComputeMultibandXWT.m
@@ -1,37 +1,47 @@
 
-function [varargout] = ComputeMultibandXWT(varargin)
-% ComputeMultibandXWT - For a given time seris, computes & displays the XWT and a variant of it
+function [varargout] = ComputeMultiBandXWT(varargin);
+% ComputeMultiBandXWT - For a given time seris, computes & displays the XWT and a variant of it
 % where only frequency peaks at each time point are kept.
-% 
-% [Wxy,Wxy_fPeaks,freq,coi,sig95] = ComputeMultibandXWT(signals,timeVec,freqRange,dj,stringency,phaseType);
-% [Wxy,Wxy_fPeaks,freq,coi,sig95] = ComputeMultibandXWT(signals,samplingInt,freqRange,dj,stringency,phaseType);
+%
+% [Wxy,Wxy_fPeaks,freq,coi,sig95] = ComputeMultiBandXWT(signals,timeVec,freqRange,dj,stringency,phaseType,plotSwitch);
+% [Wxy,Wxy_fPeaks,freq,coi,sig95] = ComputeMultiBandXWT(signals,samplingInt,freqRange,dj,stringency,phaseType,plotSwitch);
+% Inputs:
+% plotSwitch = 1; will result in plotting of the Wxy
+% plotSwitch = 2; will result in plotting of Wxy_fPeaks,
+% plotSwitch = 3; will resulti in plotting both
+
 
 if nargin < 2
-    errordlg('At least a minimum of 2 inputs reqd'); 
+    errordlg('At least a minimum of 2 inputs reqd');
+    return;
 elseif nargin < 3
-     if numel(varargin{2} < 2)
+    if numel(varargin{2} < 2)
         dt = varargin{2};
         timeVec = 0:dt:length(varargin{1})-1;
     else
         dt = mode(diff(varargin{2}));
         timeVec = varargin{2};
-     end
- freqRange = [];
- freqRange(1) = 1/length(varargin{1});
- freqRange(2) = 0.5*dt;
- dj = 1/2^5;
- stringency = 1;
- phaseType = 'all';
+    end
+    freqRange = [];
+    freqRange(1) = 1/length(varargin{1});
+    freqRange(2) = 0.5*dt;
+    dj = 1/2^5;
+    stringency = 1;
+    phaseType = 'all';
+    plotSwitch = 0;
 elseif nargin < 4
     dj = 1/2^5;
     stringency = 1;
     phaseType = 'all';
+    plotSwitch = 0;
 elseif nargin < 5
-      stringency = 1;
+    stringency = 1;
     phaseType = 'all';
-elseif nargin < 6   
+    plotSwitch = 0;
+elseif nargin < 6
     phaseType = 'all';
-elseif nargin > 6
+    plotSwitch = 0;
+elseif nargin > 7
     errordlg('Too many inputs!');
 else
     freqRange = varargin{3};
@@ -41,27 +51,51 @@ else
     dj = varargin{4};
     stringency = varargin{5};
     phaseType = varargin{6};
+    plotSwitch = varargin{7};
 end
 signal = varargin{1};
+if any(size(signal)==1)
+    signal = signal(:)';
+end
+% s1 = getspline(signal);
+% s2 = getspline(-signal);
+% s = ((s1+s2)/2)';
+
+timeVec = varargin{2};
+% f_max = findpeaks_hht(signal);
+% f_min = findpeaks_hht(signal);
+% f_all = union(f_min(:), f_max(:));
+% s1 = spline(timeVec(f_max),signal(f_max),timeVec);
+% s2 = spline(timeVec(f_min),signal(f_min),timeVec);  % Spline-method does
+% some weird stuff at high frequencies
+% s = (s1+s2)/2;
+% s = interp1(timeVec(f_all),signal(f_all), timeVec);
+signal = signal(:);
+s = signal;
+
 
 if ndims(signal)> 2
     errordlg('Input signal size cannot exceed 2 dimensions!');
 elseif size(signal,2) > 2
     errordlg('Signal input must be a matrix with no more than 2 cols, with each col being a different timeseries!')
 elseif any(size(signal)==1)
-   [Wxy,freq,coi, sig95]  = ComputeXWT(signal(:),signal(:),timeVec,freqRange,dj,stringency,phaseType);
- else
-    [Wxy,freq,coi, sig95]  = ComputeXWT(signal(:,1),signal(:,2),timeVec,freqRange,dj,stringency,phaseType);
+    [Wxy,freq,coi, sig95]  = ComputeXWT(s,s,timeVec,freqRange,dj,stringency,phaseType);
+else
+    [Wxy,freq,coi, sig95]  = ComputeXWT(s(:,1),s(:,2),timeVec,freqRange,dj,stringency,phaseType);
 end
-
+% s_norm = s/max(s);
+% s_norm = log2(s);
+% sigMat = repmat(s(:)',size(Wxy,1),1);
 R = zeros(size(Wxy));
-B = abs(Wxy);
+% B = log2(abs(Wxy)).*sigMat;
+% B = log2(abs(Wxy));
+B = abs(Wxy).^1;
 for tt = 1:size(B,2)
- blah = B(:,tt);
- dBlah = diff(blah);
- peaks = dBlah(1:end-1)>0 & dBlah(2:end)<=0;
-  peakInds = find(peaks);
- R(peakInds,tt) = B(peakInds,tt);
+    blah = B(:,tt);
+    dBlah = diff(blah);
+    peaks = dBlah(1:end-1)>0 & dBlah(2:end)<=0;
+    peakInds = find(peaks);
+    R(peakInds,tt) = B(peakInds,tt);
 end
 
 varargout{1} = Wxy;
@@ -69,3 +103,104 @@ varargout{2} = R;
 varargout{3} = freq;
 varargout{4} = coi;
 varargout{5} = sig95;
+
+switch plotSwitch
+    case 1
+        figure('Name','Wavelet Spectrogram')
+        subaxis(2,1,1),colormap(hot), hold on
+        imagesc(timeVec,log2(freq),log2(abs(Wxy)))
+        cLim = get(gca,'clim');
+        dC = diff(cLim);
+        set(gca,'clim',[cLim(1)+ 0*dC cLim(1) + 1*dC], 'xtick',[],'ydir','normal');
+        xlim([timeVec(1) timeVec(end)])
+        ylim(log2([freqRange]))
+        set(gca,'yticklabel',num2str(round(2.^(str2num(get(gca,'yticklabel'))))))
+        box off
+        for ch = 1:size(signal,2)
+            if ch == 2
+                col = 'r';
+            else
+                col = 'b';
+            end
+            subaxis(2,1,2), plot(timeVec,signal(:,1),'color',col)
+            hold on
+            set(gca,'color','k','tickdir','out'), box off
+        end
+        xlim([timeVec(1) timeVec(end)])
+        ylim([-inf inf])
+    case 2
+        figure('Name','Wavelet Spectrogram_peaks only')
+        subaxis(2,1,1),colormap(hot), hold on
+        imagesc(timeVec,log2(freq),R)
+        cLim = get(gca,'clim');
+        dC = diff(cLim);
+        set(gca,'clim',[cLim(1)+ 0*dC cLim(1) + 1*dC], 'xtick',[],'ydir','normal');
+        xlim([timeVec(1) timeVec(end)])
+        ylim(log2([freqRange]))
+        set(gca,'yticklabel',num2str(round(2.^(str2num(get(gca,'yticklabel'))))))
+        box off
+        for ch = 1:size(signal,2)
+            if ch == 2
+                col = 'r';
+            else
+                col = 'b';
+            end
+            subaxis(2,1,2), plot(timeVec,signal(:,1),'color',col)
+            hold on
+            set(gca,'color','k','tickdir','out'), box off
+        end
+        xlim([timeVec(1) timeVec(end)])
+        ylim([-inf inf])
+    case 3
+      figure('Name','Wavelet Spectrogram')
+        subaxis(2,1,1),colormap(hot), hold on
+        imagesc(timeVec,log2(freq),log2(abs(Wxy)))
+        cLim = get(gca,'clim');
+        dC = diff(cLim);
+        set(gca,'clim',[cLim(1)+ 0*dC cLim(1) + 1*dC], 'xtick',[],'ydir','normal');
+        xlim([timeVec(1) timeVec(end)])
+        ylim(log2([freqRange]))
+         set(gca,'yticklabel',num2str(round(2.^(str2num(get(gca,'yticklabel'))))))
+        box off
+        for ch = 1:size(signal,2)
+            if ch == 2
+                col = 'r';
+            else
+                col = 'b';
+            end
+            subaxis(2,1,2), plot(timeVec,signal(:,1),'color',col)
+            hold on
+            set(gca,'color','k','tickdir','out'), box off
+        end
+        xlim([timeVec(1) timeVec(end)])
+        ylim([-inf inf])  
+        
+        figure('Name','Wavelet Spectrogram_peaks only')
+        subaxis(2,1,1),colormap(hot), hold on
+        imagesc(timeVec,log2(freq),R)
+        cLim = get(gca,'clim');
+        dC = diff(cLim);
+        set(gca,'clim',[cLim(1)+ 0*dC cLim(1) + 1*dC], 'xtick',[],'ydir','normal');
+        xlim([timeVec(1) timeVec(end)])
+        ylim(log2([freqRange]))
+        set(gca,'yticklabel',num2str(round(2.^(str2num(get(gca,'yticklabel'))))))
+        box off
+        for ch = 1:size(signal,2)
+            if ch == 2
+                col = 'r';
+            else
+                col = 'b';
+            end
+            subaxis(2,1,2), plot(timeVec,signal(:,1),'color',col)
+            hold on
+            set(gca,'color','k','tickdir','out'), box off
+        end
+        xlim([timeVec(1) timeVec(end)])
+        ylim([-inf inf])
+end
+
+
+function s = getspline(x)
+N = length(x);
+p = findpeaks_hht(x);
+s = spline([0 p N+1],[0 x(p) 0],1:N);
