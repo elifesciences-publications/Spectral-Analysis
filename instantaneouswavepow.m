04d4ce62906e6cd5ae7227649f0ef02d3fafa087
Avinash Pujala <avinashpujala@gmail.com>
2015-04-23 22:57:56 -0400

    Minor code improvements

--- instantaneouswavepow.m
+++ instantaneouswavepow.m
@@ -8,10 +8,12 @@ function varargout = instantaneouswavepow(Wxy)
 % Wxy - matrix of crosswavelet coefficients across of size w x n, where
 %       w is number of frequency points, and n is the number of time pts.
 % Outputs:
-% totPow - Pan-spectral total power at each point n.
+% totPow - Pan-spectral total power at each point n, normalized by # of
+%          wavelet scales
 % totPow_maxNorm - Max-normalized version of totPow
 
-totPow = sum(abs(Wxy),1);
+
+totPow = sum(abs(Wxy),1)./repmat(size(Wxy,1),1,size(Wxy,2));
 totPow_maxNorm = totPow/max(totPow);
 
 varargout{2} = totPow;
