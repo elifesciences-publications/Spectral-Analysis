5626d817b54a3b0b8f9f8fd8085a5d8ef9f05469
Avinash Pujala <avinashpujala@gmail.com>
2015-05-23 02:42:06 -0400

    Color bar added, and other fixes

--- Cross wavelet/PlotWxy.m
+++ Cross wavelet/PlotWxy.m
@@ -1,31 +1,55 @@
-function PlotWxy(varargin)
-%PlotWxy - Function that accepts a matrix of crosswavelet coefficients and
+function varargout = PlotWxy(varargin)
+%PlotWxy  Function that accepts a matrix of crosswavelet coefficients and
 %          plots its image
 %
 % PlotWxy(Wxy)
-% PlotWxy(Wxy,time)
-% PlotWxy(Wxy,time,freq)
-% PlotWxy(Wxy,time,freq,[x,y])
+% PlotWxy(...,time)
+% PlotWxy(...,freq)
+% PlotWxy(...,[x,y])
+% PlotWxy(...,clim)
+% fh = PlotWxy(...)
+%
+% Inputs:
+% Wxy - Crosswavelet coeff matrix
+% time - Time vector corresponding to the col entries in Wxy
+% freq - Freq vector corresponding to row entries in Wxy
+% [x,y] - Signals that produced Wxy; in included, these will be plot in separate
+%   ...axes under near Wxy.
+% clim - Color limits; A 2-element array that specifies the lower and upper
+%   ... limits of the color scale
 
 Wxy = varargin{1};
-if nargin < 3
-    error('Minimum of 3 inputs reqd')
-    return;
-elseif nargin == 3
+if nargin < 2
+    time = 1:size(Wxy,1);
+    freq = 1:size(Wxy,1);
+elseif nargin < 3
+    time = varargin{2};
+    freq = 1:size(Wxy,1);
+elseif nargin < 4
     time = varargin{2};
     freq = varargin{3};
-elseif nargin == 4
+elseif nargin ==4
     time = varargin{2};
     freq = varargin{3};
-    x = varargin{4};
-    y = x;
+    sigs = varargin{4};
 elseif nargin == 5
+    if strcmpi(varargin{4},'clim')
+        time = varargin{2};
+        freq= varargin{3};
+        sigs =[];
+        cLim = varargin{5};
+    else
+        error('Color limits not specified properly')
+        return;
+    end
+elseif nargin ==6
     time = varargin{2};
-    freq = varargin{3};
-    x = varargin{4};
-    y = varargin{5};
+    freq= varargin{3};
+    sigs = varargin{4};
+    cLim = varargin{6};
 else
-    error('Incorrect of inputs!')
+    error('Incorrect # of inputs')
+    return
 end
 
 
@@ -40,24 +64,19 @@ pkDetThr = 0.1;
 
 
 %%%%%% XWT Axes
-% aWxy = angle(Wxy);
-% Wxy(abs(aWxy)< pi/2) = 0;
 cData = Wxy;
 cData(cData==0) = NaN;
-
 dt = time(2)-time(1);
 
-
-powerSpectrum = sum(abs(Wxy),2);
+powerSpectrum = sum(abs(Wxy),2)/size(Wxy,2);
 normPowerSpectrum = powerSpectrum/max(powerSpectrum);
 logPowerSpectrum = log2(powerSpectrum);
+logPowerSpectrum(isinf(logPowerSpectrum))= nan;
 normLogPowerSpectrum = logPowerSpectrum/max(logPowerSpectrum);
 [maxtab,~] = peakdet(normLogPowerSpectrum,pkDetThr);
-if numel(maxtab) == 0
-    maxtab(:,1) = 1;
-    maxtab(:,2)= 0.5;
+if isempty(maxtab)
+    maxtab(1:2) = nan;
 end
-
 
 figPos = get(gcf,'position');
 set(gcf,'position',[figPos(1) figPos(2) figPos(3)*1.2 figPos(4)])
@@ -75,12 +94,11 @@ else
     set(HCB,'yticklabel',barylbls);
 end
 
-
-if nargin == 3
+if ((nargin == 3) || isempty(sigs))
     aPos1 = [0.8 1 0 0]; % Wavelet axes
     aPos2 = [0.2 1 0.8 0]; % Global power spectrum axes - linear plot
     aPos2b  = aPos2; % Global power spectrum axes - log plot
-    axHandles = CreateSubaxes(gcf,aPos1, aPos2, aPos2b);
+    axHandles = CreateSubaxes(gcf, aPos1, aPos2, aPos2b);
 elseif nargin > 3
     aPos1 = [0.8 0.8 0 0.2]; % Wavelet axes
     aPos2 = [0.2 0.8 0.8 0.2]; % Global power spectrum axes - linear plot
@@ -97,13 +115,27 @@ ax2b = axHandles(3);
 Yticks = 2.^(fix(log2(min(freq))):fix(log2(max(freq))));
 axes(ax1), hold on
 H = imagesc(time,log2(freq),abs(log2(cData)));
-clim =get(gca,'clim'); % center color limits around log2(1)=0
-clim = [0 0.9]*max(clim(2));
-set(gca,'clim',clim,'YLim',log2([min(freq),max(freq)]), ...
-    'YDir','normal', ...
-    'YTick',log2(Yticks(:)), ...
-    'YTickLabel',num2str(Yticks'), ...
-    'layer','top','xtick',[])
+if (nargin)
+end
+if ~exist('cLim')
+    cLim =get(gca,'clim'); % center color limits around log2(1)=0
+    cLim = [0 0.9]*max(cLim(2));
+end
+safecolorbar;
+
+if  isempty(sigs)
+      set(gca,'clim',cLim,'YLim',log2([min(freq),max(freq)]), ...
+        'YDir','normal', ...
+        'YTick',log2(Yticks(:)), ...
+        'YTickLabel',num2str(Yticks'), ...
+        'layer','top')
+else
+      set(gca,'clim',cLim,'YLim',log2([min(freq),max(freq)]), ...
+        'YDir','normal', ...
+        'YTick',log2(Yticks(:)), ...
+        'YTickLabel',num2str(Yticks'), ...
+        'layer','top','xtick',[])
+end
 
 ylabel('Frequency (Hz)')
 set(ax1,'color','k','ycolor','k')
@@ -121,7 +153,8 @@ ylpos = get(yl,'pos');
 colormap('jet')
 
 % Plotting phases
-aWxy = angle(Wxy);
+% aWxy = angle(Wxy);
+aWxy = angle(cData);
 phs_dt=round(length(time)/arrowDensity(1));
 tidx=max(floor(phs_dt/2),1):phs_dt:length(time);
 phs_dp=round(length(freq)/arrowDensity(2));
@@ -140,19 +173,21 @@ hold off
 %% XW Power Spectrum Axes
 axes(ax2); hold on
 set(ax2,'color','w');
-xlabel([{'Normalized'}; {'Power'}])
+xlabel('Power')
 peakFreqs = freq(maxtab(:,1));
-if strcmpi(yScale,'linear'), plot(normPowerSpectrum, freq,'k','linewidth',2);
+if strcmpi(yScale,'linear'), plot(powerSpectrum, freq,'k','linewidth',2);
     hold on
-    plot(normLogPowerSpectrum, freq,'k:','linewidth',2)
+    plot(logPowerSpectrum, freq,'k:','linewidth',2)
     logPeakFreqs = peakFreqs;
 else
+    %     plot(normPowerSpectrum,log2(freq),'k','linewidth',2)
     plot(normPowerSpectrum,log2(freq),'k','linewidth',2)
     hold on
     plot(normLogPowerSpectrum,log2(freq),'k:','linewidth',2)
     logPeakFreqs = log2(peakFreqs);
 end
-set(ax2,'ylim',ylims1,'xlim',[0 1],'xtick',[0.5 1],'ytick',[])
+% set(ax2,'ylim',ylims1,'xlim',[0 1],'xtick',[0.5 1],'ytick',[])
+set(ax2,'ylim',ylims1,'xlim',[-inf inf],'ytick',[])
 ylims2 = get(ax2,'ylim');
 
 xvals = 0.2*ones(size(peakFreqs)); % X coordinates for text displaying peak frequencies
@@ -165,24 +200,26 @@ text(xvals,logPeakFreqs,txt,'color','r');
 axes(ax2b)
 set(ax2b,'xaxislocation','top','yaxislocation','right',...
     'color','none','xscale','log','ytick',[],'ycolor','w');
-xt = [0.25 0.5 1];
-set(ax2b,'xtick',xt,'xlim',[0 1]);
+% xt = [0.25 0.5 1];
+% set(ax2b,'xtick',xt,'xlim',[-inf inf]);
+set(ax2b,'xlim',[min(logPowerSpectrum) max(logPowerSpectrum)]);
 leg = {'Linear';'Log'};
 legend(ax2,leg)
 
-if nargin > 3
+if ((nargin > 3) && (~isempty(sigs)))
     axes(ax3)
-    plot(time,x,'b')
-    hold on
-    plot(time,y,'r')
+    plot(time,sigs(:,1),'b')
+    if size(sigs,2) ==2
+        hold on
+        plot(time,sigs(:,2),'r')
+    end
     xlim([time(1) time(end)])
     xlabel('Time(s)')
- end
-
+end
 
 
 function hcb = safecolorbar(varargin)
-colorbarlocation = 'NorthOutside'; % AP code. Default = EastOutside (type help colorbar for more location options)
+colorbarlocation = 'North'; % Default = EastOutside (type help colorbar for more location options)
 vv=sscanf(version,'%i.');
 if strcmpi(colorbarlocation,'North')
     hcb=colorbar(varargin{:},'Location',colorbarlocation,'Box','on','Xcolor','k','YColor','k');
