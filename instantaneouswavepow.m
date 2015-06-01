ce4698413920590550630de4d92a76095409e990
Avinash Pujala <avinashpujala@gmail.com>
2015-06-01 19:42:57 -0400

    Now also computes max pow at each time pt

--- instantaneouswavepow.m
+++ instantaneouswavepow.m
@@ -3,7 +3,7 @@ function varargout = instantaneouswavepow(Wxy)
 % INSTANTANEOUSWAVEPOW - Computes the total XW power across all frequencies
 %                        at each instant in time.
 % totPow_maxNorm  = instantaneouswavepow(Wxy);
-% [totPow_maxNorm, totPow] = instantaneouswavepow(Wxy);
+% [totPow_maxNorm, totPow, maxPow] = instantaneouswavepow(Wxy);
 % Inputs:
 % Wxy - matrix of crosswavelet coefficients across of size w x n, where
 %       w is number of frequency points, and n is the number of time pts.
@@ -11,9 +11,13 @@ function varargout = instantaneouswavepow(Wxy)
 % totPow - Pan-spectral total power at each point n, normalized by # of
 %          wavelet scales
 % totPow_maxNorm - Max-normalized version of totPow
+% maxPow - 
 
 totPow = mean(abs(Wxy),1);
 totPow_maxNorm = totPow/max(totPow);
 
+
+
 varargout{2} = totPow;
 varargout{1} = totPow_maxNorm;
+varargout{3} = max(abs(Wxy),[],1);
